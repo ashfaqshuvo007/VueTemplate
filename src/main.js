e1790ee4e5cd71ea467815ee552fe7681251e880
Ashfaq H Ahmed <ashfaqshuvo14@gmail.com>
2020-04-03 21:22:32 +0600

    Vuetifie the template

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/css/styles.css';
+import './assets/css/bootstrap.css';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
